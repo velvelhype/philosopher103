b9f8f5a9e7899df2c4528b3e9586d3241eb88256
velvelhype <velvelhype@gmail.com>
2022-01-11 12:11:55 +0900

    ai

--- 2_philos.c
+++ 2_philos.c
@@ -2,10 +2,19 @@
 #include <pthread.h>
 #include <stdbool.h>
 
+
+void *philo(void *p)
+{
+
+}
+
 int main()
 {
 	pthread_t philo1;
 	pthread_t philo2;
-	bool fork[2] = {1, 0};
-	printf("%d\n", fork[0], fork[1]);
+	bool fork[2] = {1, 1};
+	printf("%d %d\n", fork[0], fork[1]);
+	fork[0] = 0;
+	printf("%d %d\n", fork[0], fork[1]);
+	
 }
