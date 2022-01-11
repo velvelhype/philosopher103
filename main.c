e65c011e5f963a27b267ad79768f9207e07e4da5
velvelhype <velvelhype@gmail.com>
2022-01-12 04:34:09 +0900

    lets make prototype

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdbool.h>
+
+
+void *philo(void *p)
+{
+
+}
+
+void	init_status()
+{
+
+}
+
+int main()
+{
+	//init_status();
+	//start_philos();
+
+}
