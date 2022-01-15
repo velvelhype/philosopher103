2115830202ef2d96add4d48cb279d94874046425
velvelhype <velvelhype@gmail.com>
2022-01-15 16:20:12 +0900

    deep

--- 3philos.c
+++ 3philos.c
@@ -1,6 +1,6 @@
 #include "philo.h"
 
-#define MAX  4
+#define MAX 5
 
 typedef struct s_mut
 {
@@ -61,56 +61,55 @@ int	fork_number(int fork_number)
 
 void take_a_fork(t_mut *status, int code_number)
 {
-	pthread_mutex_lock(&status->fork_mutex[fork_number(code_number)]);
-	pthread_mutex_lock(&status->fork_mutex[fork_number(code_number + 1)]);
+	// pthread_mutex_lock(&status->fork_mutex[fork_number(code_number)]);
+	// pthread_mutex_lock(&status->fork_mutex[fork_number(code_number + 1)]);
+	// printf_time();
+	// printf("code : %d\n", code_number);
 	while(1)
 	{
 		if (code_number % 2)
 		{
-			if(status->forks[fork_number(code_number + 1)])
+			if(status->forks[fork_number(code_number + 1)]
+			 && status->forks[fork_number(code_number)])
 			{
 				status->forks[fork_number(code_number + 1)] = 0;
-				printf("%d ", fork_number(code_number + 1));
+				// printf("%d ", fork_number(code_number + 1));
 				action("fork", status, code_number);
-				while (1)
-				{
-				if (status->forks[fork_number(code_number)])
-				{
+				// while (1)
+				// {
+
+				// if (status->forks[fork_number(code_number)])
+				// {
 					status->forks[fork_number(code_number)] = 0;
-					printf("%d ", fork_number(code_number));
+					// printf("%d ", fork_number(code_number));
 					action("fork", status, code_number);
 					action("eat", status, code_number);
 					status->forks[fork_number(code_number)] = 1;
 					status->forks[fork_number(code_number + 1)] = 1;
-					pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number)]);
-					pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number + 1)]);
+					// pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number)]);
+					// pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number + 1)]);
 					return ;
-				}
-				}
+				// }
+				// }
 			}
 		}
 		else 
 		{
-			if(status->forks[fork_number(code_number)])
+			if(status->forks[fork_number(code_number)] && 
+			status->forks[fork_number(code_number + 1)])
 			{
 				status->forks[fork_number(code_number)] = 0;
-				printf("%d ", fork_number(code_number));
+				// printf("%d ", fork_number(code_number));
 				action("fork", status, code_number);
-				while(1)
-				{
-				if (status->forks[fork_number(code_number + 1)])
-				{
 					status->forks[fork_number(code_number + 1)] = 0;
-					printf("%d ", fork_number(code_number + 1));
+					// printf("%d ", fork_number(code_number + 1));
 					action("fork", status, code_number);
 					action("eat", status, code_number);
 					status->forks[fork_number(code_number)] = 1;
 					status->forks[fork_number(code_number + 1)] = 1;
-					pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number)]);
-					pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number + 1)]);
+					// pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number)]);
+					// pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number + 1)]);
 					return ;
-				}
-				}
 			}
 		}
 	}
@@ -124,52 +123,63 @@ void	*philo_life(void *p)
 	while(1)
 	{
 		take_a_fork(status, code_number);
-		// action("eat", status, code_number);
 		action("sleep", status, code_number);
 		action("think", status, code_number);
-
 	}
 	return NULL;
 }
 
 int main()
 {
+	pthread_t *philos;
+	philos = (pthread_t *)malloc(sizeof(pthread_t) * MAX);
+
 	pthread_t philo1;
 	pthread_t philo2;
 	pthread_t philo3;
-
 	pthread_t philo4;
+	pthread_t philo5;
 
 	t_mut status;
 	//init
-	status.number = 4;
-	// status.fork = 1;
+	status.number = MAX;
 	for (int i = 0; i < MAX; i++)
 		status.forks[i] = 1;
 	status.act_count = 0;
 	pthread_mutex_init(&status.talk_mtx, NULL);
 	pthread_mutex_init(&status.act_mtx, NULL);
-	// pthread_mutex_init(&status.a_fork_mtx, NULL);
 	for (int i = 0; i < MAX; i++)
 		pthread_mutex_init(&status.fork_mutex[i], NULL);
-
-		pthread_create(&philo1
+	//create threads
+	for (int i = 0; i < MAX; i++)
+	{
+		pthread_create(&philos[i]
 		, NULL, &philo_life, &status);
-		pthread_detach(philo1);
+		pthread_detach(philos[i]);
 		usleep(100);
+	}
 
-		pthread_create(&philo2
-		, NULL, &philo_life, &status);
-		pthread_detach(philo2);
-		usleep(100);
+	// 	pthread_create(&philo1
+	// 	, NULL, &philo_life, &status);
+	// 	pthread_detach(philo1);
+	// 	usleep(100);
 
-		pthread_create(&philo3
-		, NULL, &philo_life, &status);
-		pthread_detach(philo3);
+	// 	pthread_create(&philo2
+	// 	, NULL, &philo_life, &status);
+	// 	pthread_detach(philo2);
+	// 	usleep(100);
+
+	// 	pthread_create(&philo3
+	// 	, NULL, &philo_life, &status);
+	// 	pthread_detach(philo3);
 	
-		pthread_create(&philo4
-		, NULL, &philo_life, &status);
-		pthread_detach(philo4);
+	// 	pthread_create(&philo4
+	// 	, NULL, &philo_life, &status);
+	// 	pthread_detach(philo4);
+
+	// 	pthread_create(&philo5
+	// 	, NULL, &philo_life, &status);
+	// 	pthread_detach(philo5);
 	//
 	usleep(1000000);
 	// printf("acted %d\n", status.act_count);
