cc428b7e0487ad8906c169f2ec0e2a439b00ed4e
velvelhype <velvelhype@gmail.com>
2022-01-16 04:33:02 +0900

    argv1

--- 3philos.c
+++ 3philos.c
@@ -4,14 +4,14 @@
 
 typedef struct s_mut
 {
-	pthread_mutex_t fork_mutex[MAX];
-	int forks[MAX];
-	pthread_mutex_t act_mtx;
-	pthread_mutex_t talk_mtx;
+	int max_number;
 	int number;
-	int act_count;
 	int	time_to_eat;
 	int	time_to_die;
+	int forks[MAX];
+	pthread_mutex_t fork_mutex[MAX];
+	pthread_mutex_t act_mtx;
+	pthread_mutex_t talk_mtx;
 } t_mut;
 
 void action(char *message, t_mut *status, int code_number)
@@ -52,28 +52,28 @@ int fork_number(int fork_number)
 	return (fork_number);
 }
 
-void take_a_fork(t_mut *status, int code_number)
+void take_a_fork(t_mut *stat, int code_number)
 {
 	// printf_time();
 	// printf("code : %d\n", code_number);
 	while (1)
 	{
-		if (status->forks[fork_number(code_number)] &&
-			status->forks[fork_number(code_number + 1)])
+		if (stat->forks[fork_number(code_number)] &&
+			stat->forks[fork_number(code_number + 1)])
 		{
-			pthread_mutex_lock(&status->fork_mutex[fork_number(code_number)]);
-			pthread_mutex_lock(&status->fork_mutex[fork_number(code_number + 1)]);
-			status->forks[fork_number(code_number + 1)] = 0;
+			pthread_mutex_lock(&stat->fork_mutex[fork_number(code_number)]);
+			pthread_mutex_lock(&stat->fork_mutex[fork_number(code_number + 1)]);
+			stat->forks[fork_number(code_number + 1)] = 0;
 			// printf("%d ", fork_number(code_number + 1));
-			action("fork", status, code_number);
-			status->forks[fork_number(code_number)] = 0;
+			action("fork", stat, code_number);
+			stat->forks[fork_number(code_number)] = 0;
 			// printf("%d ", fork_number(code_number));
-			action("fork", status, code_number);
-			action("eat", status, code_number);
-			status->forks[fork_number(code_number)] = 1;
-			status->forks[fork_number(code_number + 1)] = 1;
-			pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number)]);
-			pthread_mutex_unlock(&status->fork_mutex[fork_number(code_number + 1)]);
+			action("fork", stat, code_number);
+			action("eat", stat, code_number);
+			stat->forks[fork_number(code_number)] = 1;
+			stat->forks[fork_number(code_number + 1)] = 1;
+			pthread_mutex_unlock(&stat->fork_mutex[fork_number(code_number)]);
+			pthread_mutex_unlock(&stat->fork_mutex[fork_number(code_number + 1)]);
 			return;
 		}
 	}
@@ -81,41 +81,49 @@ void take_a_fork(t_mut *status, int code_number)
 
 	void *philo_life(void *p)
 	{
-		t_mut *status = p;
-		status->number -= 1;
-		int code_number = status->number;
+		t_mut *stat = p;
+		stat->number -= 1;
+		int code_number = stat->number;
 		while (1)
 		{
-			take_a_fork(status, code_number);
-			action("sleep", status, code_number);
-			action("think", status, code_number);
+			take_a_fork(stat, code_number);
+			action("sleep", stat, code_number);
+			action("think", stat, code_number);
 		}
 		return (NULL);
 	}
 
-	int main(int argc, char **argv)
+	void	init_status(t_mut *stat, char **argv)
 	{
 		//init
+		printf("where %s\n", argv[1]);
+		stat->number = ft_atoi(argv[1]);
+		printf("where\n");
+		stat->max_number = ft_atoi(argv[1]);
+		for (int i = 0; i < MAX; i++)
+			stat->forks[i] = 1;
+		pthread_mutex_init(&stat->talk_mtx, NULL);
+		for (int i = 0; i < MAX; i++)
+			pthread_mutex_init(&stat->fork_mutex[i], NULL);
+		//create threads
+	}
+
+	int main(int argc, char **argv)
+	{
 		pthread_t *philos;
 		philos = (pthread_t *)malloc(sizeof(pthread_t) * MAX);
-		t_mut status;
-		status.number = MAX;
-		for (int i = 0; i < MAX; i++)
-			status.forks[i] = 1;
-		status.act_count = 0;
-		pthread_mutex_init(&status.talk_mtx, NULL);
-		pthread_mutex_init(&status.act_mtx, NULL);
-		for (int i = 0; i < MAX; i++)
-			pthread_mutex_init(&status.fork_mutex[i], NULL);
-		//create threads
+		t_mut stat;
+		if(argc != 5 && argc != 6)
+			error();
+		init_status(&stat, argv);
 		for (int i = 0; i < MAX; i++)
 		{
-			pthread_create(&philos[i], NULL, &philo_life, &status);
+			pthread_create(&philos[i], NULL, &philo_life, &stat);
 			// pthread_join(philos[i], NULL);
 			pthread_detach(philos[i]);
 			usleep(100);
 		}
 		//./a.out
-		// usleep(500000);
-		usleep(500);
+		usleep(500000);
+		// usleep(50000);
 	}
